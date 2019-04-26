43604c970402e92d144ea7333ec2a58410c2a528
Aldbas <44149366+Aldbas@users.noreply.github.com>
2019-04-26 10:35:49 -0700

    flutter logo breathing tween animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,70 @@
+import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(LogoApp());
+
+class AnimatedLogo extends AnimatedWidget {
+  AnimatedLogo ({Key key, Animation<double> animation})
+      : super(key:key, listenable: animation);
+  Widget build(BuildContext context) {
+    final Animation<double> animation = listenable;
+    return Center(
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10),
+        height: animation.value,
+        width:  animation.value,
+        child: FlutterLogo(),
+      ),
+    );
+  }
+}
 
 class LogoApp extends StatefulWidget {
   _LogoAppState createState() => _LogoAppState();
 }
 
-class _LogoAppState extends State<LogoApp> {
+class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController controller;
+
+
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        margin: EdgeInsets.symmetric(vertical: 10),
-        height: 300,
-        width: 300,
-        child: FlutterLogo(),
-      ),
-    );
+
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(duration: const Duration(seconds: 3), vsync: this);
+    animation = Tween<double>(begin: 0, end:600).animate(controller)
+
+    ..addStatusListener((status){
+      if(status == AnimationStatus.completed) {
+        controller.reverse();
+      } else if (status == AnimationStatus.dismissed){
+        controller.forward();
+      }
+    })
+    ..addStatusListener((state) => print('$state'));
+//    ..addListener((){
+//      setState(() {
+//
+//      });
+//    });
+    controller.forward();
+  }
+  @override
+  Widget build(BuildContext context) => AnimatedLogo(animation: animation);
+//    return Center(
+//        child: Container(
+//          margin: EdgeInsets.symmetric(vertical: 10),
+//          height: animation.value,
+//          width: animation.value,
+//          child: FlutterLogo(),
+//      ),
+//    );
+//  }
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 }
